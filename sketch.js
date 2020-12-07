b598f05d125e81d0b798bcd125d3f596a849b856
zazonf <65169143+zazonf@users.noreply.github.com>
2020-12-07 15:01:18 -0300

    fix round(for real this time)

--- sketch.js
+++ sketch.js
@@ -2,41 +2,39 @@ var ang = 0;
 var slider;
 function setup() {
   createCanvas(windowWidth, windowHeight - 50);
-  slider = createSlider(0,PI, PI*2, 0.01)
+  slider = createSlider(0, PI, PI * 2, 0.01)
 }
-
 function draw() {
   resizeCanvas(windowWidth, windowHeight - 30);
   ang = slider.value();
   background(127);
   noStroke();
   fill(50);
-  translate(0,0);
+  translate(0, 0);
   textSize(32);
-  text(round((ang * (2*180) / PI, 20,40),0))
-  text(ang, 20,80)
-  translate(width/2,height);
+  var unrounded = ang * (2 * 180) / PI;
+  var rounded = round(unrounded)
+  text(rounded, 20, 40)
+  text(ang, 20, 80)
+  translate(width / 2, height);
   //2×180°/π
   fill(255);
   stroke(255);
-  branch(height/5);
+  branch(height / 5);
 }
-
 function branch(len)
 {
-  line(0,0,0,-len);
-  translate(0, -len)
-  if(len > 4)
+  line(0, 0, 0, - len);
+  translate(0, - len)
+  if (len > 4)
   {
     push();
     rotate(ang);
     branch(len * 0.67)
     pop();
     push();
-    rotate(-ang);
+    rotate( - ang);
     branch(len * 0.67)
-    pop();  
+    pop();
   }
-  
-  
 }
